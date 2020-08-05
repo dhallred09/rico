bbc67acf9a12b07ac732ec35fe72e4aab88acaf5
dhallred09 <58126907+dhallred09@users.noreply.github.com>
2020-08-05 00:01:28 -0600

    setup with npm init
    Added "Hi Mom" and Quit button.

--- toyapp/src/App 2.js
+++ toyapp/src/App 2.js
@@ -3,13 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  console.log("hi Daddy!");
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <text>HI MOM</text>
-        <button>QUIT</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
