3fb3812d3a0e4d052e968325d3467d493c36025e
dhallred09 <58126907+dhallred09@users.noreply.github.com>
2020-08-07 22:35:39 -0600

    welcome page with tic-tac-toe page

--- toyapp/App.js
+++ toyapp/App.js
@@ -14,6 +14,7 @@ import {
   View,
   Text,
   StatusBar,
+  Button,
 } from 'react-native';
 
 import {
@@ -23,53 +24,83 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-// import {View}from 'React-native';
 
-const App: () => React$Node = () => {
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+function HomeScreen({ navigation }) {
   return (
-    <>
+    <View style={styles.sectionContainer}>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}>
-          <Header />
+          {/* <Header />
           {global.HermesInternal == null ? null : (
             <View style={styles.engine}>
               <Text style={styles.footer}>Engine: Hermes</Text>
             </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One DADDY Rocks Again</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+          )}              */}
+          <View style={styles.sectionContainer}>
+            <Button 
+              onPress={() => navigation.navigate('TicTacToe')}
+              title="Run Tic-Tac-Toe"
+              color="#841584"
+              backgroundColor = '#34eb74'
+              >
+            </Button>
           </View>
         </ScrollView>
       </SafeAreaView>
-    </>
+    </View>
+  );
+}
+function HomeScreen2() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+function TicTacScreen() {
+  return (
+    <View style={styles.TTTcontainer}>
+      <View style={{flexDirection: "row"}}>
+        <View style={[styles.TTTtile, { borderLeftWidth: 0, borderTopWidth: 0} ]} />
+        <View style={[styles.TTTtile, { borderTopWidth: 0 }]} />
+        <View style={[styles.TTTtile, { borderTopWidth: 0, borderRightWidth: 0} ]} />
+      </View>
+    
+      <View style={{flexDirection: "row"}}>
+        <View style={[styles.TTTtile, { borderLeftWidth: 0 }]} />
+        <View style={[styles.TTTtile,]} />
+        <View style={[styles.TTTtile, { borderRightWidth: 0 }]} />
+      </View>
+    
+      <View style={{flexDirection: "row"}}>
+        <View style={[styles.TTTtile, { borderLeftWidth: 0, borderBottomWidth: 0} ]} />
+        <View style={[styles.TTTtile, { borderBottomWidth: 0 }]} />
+        <View style={[styles.TTTtile, { borderRightWidth: 0, borderBottomWidth: 0} ]} />
+      </View>
+    </View>
+  );
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen 
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome'}}
+        />
+        <Stack.Screen name="TicTacToe" component={TicTacScreen} /> 
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
@@ -110,6 +141,17 @@ const styles = StyleSheet.create({
     paddingRight: 12,
     textAlign: 'right',
   },
+  TTTcontainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  TTTtile: {
+    borderWidth: 10,
+    width: 100,
+    height: 100,
+  }
 });
 
 export default App;
